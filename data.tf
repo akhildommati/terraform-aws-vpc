7b480413f0881e981db8684030b01b48384f930c
akhil <akhildommati567@gmail.com>
2026-02-09 19:42:54 -0500

    vpc_peering

--- data.tf
+++ data.tf
@@ -6,3 +6,13 @@ data "aws_vpc" "default" {
   
   default = true
 }
+
+data "aws_route_table" "main" {
+  vpc_id = local.default_vpc_id
+  filter {
+    name   = "association.main"
+    values = ["true"]
+    
+  }
+  
+}
