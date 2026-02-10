c788a9b2d06cd16ebd4c92f130719a7e382595e6
akhil <akhildommati567@gmail.com>
2026-02-09 21:13:44 -0500

    vpc-peering

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,31 @@
 #     value = data.aws_availability_zones.available
   
 # }
+
+output "vpc_id" {
+    value = aws_vpc.main.id
+}
+
+output "public_route_table_id" {
+    value = aws_route_table.public.id
+}
+
+output "private_route_table_id" {
+    value = aws_route_table.private.id
+}
+
+output "database_route_table_id" {
+    value = aws_route_table.database.id
+}
+
+output "public_subnet_ids" {
+    value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+    value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+    value = aws_subnet.database[*].id
+}
